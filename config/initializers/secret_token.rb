7ae48ec0a2f4a6bac13b72070fdb60cb3c42eed9
Jonathan Mumm <mumm@tokbox.com>
2011-09-23 23:14:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftmunk::Application.config.secret_token = '47865cc19b606e923722266d22ab7f0197f6c89bd9135c35c544fcfde1a1563f6b1aab27d98f5f67b3ca64b4db6c03b76a35551bdb59ae5570917f073da01f49'
