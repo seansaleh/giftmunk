96b0bf921c717bf0b7406f3494f64ced9a02b60c
Sean Saleh <Sean@sean>
2011-09-24 20:19:39 -0700

    full amazon code

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -3,7 +3,8 @@ require 'amazon_product'
 class ReferralsController < ApplicationController
   
   def index
- search_term = 'eric reis'
+# replace search_term with your variable from hunch
+ search_term = 'pencil'
     req = AmazonProduct["us"]
   
     
@@ -17,11 +18,20 @@ class ReferralsController < ApplicationController
     :keywords => search_term, 
     :response_group => 'Offers')
     
-    hashed_resp = resp.to_hash
-    #puts hashed_resp
-    render :text => hashed_resp["Items"]["Item"][0]["OfferSummary"]["LowestNewPrice"]["FormattedPrice"]
-    #render :text => hashed_resp["Items"]["Item"][0]["Offers"]["MoreOffersUrl"]
-
+    initial_resp = resp.to_hash
+    product_asin = initial_resp["Items"]["Item"][0]["ASIN"]
+    product_url = initial_resp["Items"]["Item"][0]["Offers"]["MoreOffersUrl"]
+    product_price = initial_resp["Items"]["Item"][0]["OfferSummary"]["LowestNewPrice"]["FormattedPrice"]
+    
+    image_resp = req.find(product_asin, :response_group => 'Images').to_hash
+    image_medium_url = image_resp["Items"]["Item"]["MediumImage"]["URL"]
+    image_medium_x = image_resp["Items"]["Item"]["MediumImage"]["Width"]["__content__"]
+    image_medium_y = image_resp["Items"]["Item"]["MediumImage"]["Height"]["__content__"]
+    image_large_url = image_resp["Items"]["Item"]["LargeImage"]["URL"]
+    image_large_x = image_resp["Items"]["Item"]["LargeImage"]["Width"]["__content__"]
+    image_large_y = image_resp["Items"]["Item"]["LargeImage"]["Height"]["__content__"]
+    
+    render :text => "ASIN: " + product_asin + ", URL: " + product_url + " , Price: " + product_price + ", Medium URL: " + image_medium_url + " , X: " + image_medium_x + ", Y: " + image_medium_y + ", Large URL: " + image_large_url + " , X: " + image_large_x + ", Y: " + image_large_y
 
 end
 
